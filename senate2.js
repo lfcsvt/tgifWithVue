7cbe710093f7903fc2f6982529abdc367ed5a1da
Lucas Fernando Correa de Souza <lucasfernandocorreadesouza@MacBook-Pro-de-lfcsvt.local>
2018-11-20 17:25:27 +0100

    newVueFuntions

---
+++ senate2.js
@@ -0,0 +1,138 @@
+const app = new Vue({
+    el: '#tgifApp',
+    data: {
+        congressmen: [],
+        leastLoyal: [],
+        mostLoyal: [],
+        leastEngaged: [],
+        mostEngaged: []
+
+    },
+    created: function () {
+        fetch('https://api.propublica.org/congress/v1/113/senate/members.json', {
+            headers: new Headers({
+                'X-API-Key': 'XiS3U2dJVOswHAgjUcMNIBkG4aANYLQIBCowZeNB'
+            })
+        }).then(response => response.json()).then(response => {
+            this.congressmen = response.results[0].members
+            return this.congressmen
+        });
+
+    },
+    computed: {
+        getLeastLoyal() {
+            var result = []
+            var percentage = this.congressmen.length / 100 * 10
+            this.congressmen.forEach(function (congressman) {
+                result.push(congressman.votes_with_party_pct)
+            })
+            result.sort((a, b) => a - b)
+            result = result.slice(0, percentage)
+            var breakPoint = Math.max(...result);
+            this.leastLoyal = this.congressmen.filter(member => member.votes_with_party_pct <= breakPoint)
+            return this.leastLoyal
+
+        },
+        getMostLoyal() {
+            var result = []
+            var percentage = this.congressmen.length / 100 * 10
+            this.congressmen.forEach(function (congressman) {
+                result.push(congressman.votes_with_party_pct)
+            })
+            result.sort((a, b) => b - a)
+            result = result.slice(0, percentage)
+            var breakPoint = Math.min(...result);
+            this.mostLoyal = this.congressmen.filter(member => member.votes_with_party_pct >= breakPoint)
+            return this.mostLoyal
+
+        },
+
+        getMostEngaged() {
+            var result = []
+            var percentage = this.congressmen.length / 100 * 10
+            this.congressmen.forEach(function (congressman) {
+                result.push(congressman.missed_votes_pct)
+            })
+            result.sort((a, b) => a - b)
+            result = result.slice(0, percentage)
+            var breakPoint = Math.max(...result);
+            this.leastEngaged = this.congressmen.filter(member => member.missed_votes_pct <= breakPoint)
+            return this.leastEngaged
+
+        },
+
+        getLeastEngaged() {
+            var result = []
+            var percentage = this.congressmen.length / 100 * 10
+            this.congressmen.forEach(function (congressman) {
+                result.push(congressman.missed_votes_pct)
+                console.log(congressman)
+            })
+            result.sort((a, b) => b - a)
+            result = result.slice(0, percentage)
+            var breakPoint = Math.min(...result);
+            this.mostEngaged = this.congressmen.filter(member => member.missed_votes_pct >= breakPoint)
+            return this.mostEngaged
+
+        },
+
+        createArray() {
+            var array = [{
+                    name: 'Democrats',
+                    numberOfMembers: this.count("D"),
+                    votesPercentage: this.percentVoted("D")
+                },
+                {
+                    name: 'Republicans',
+                    numberOfMembers: this.count("R"),
+                    votesPercentage: this.percentVoted("R")
+                },
+                {
+                    name: 'Independents',
+                    numberOfMembers: this.count("I"),
+                    votesPercentage: this.percentVoted("I")
+                },
+                {
+                    name: "Total",
+                    numberOfMembers: this.count("I") + this.count("D") + this.count("R"),
+                    votesPercentage: this.totalPercentage().toFixed(2)
+                }
+                 ]
+            return array
+
+        }
+
+    },
+
+    methods: {
+        count(partyCode) {
+            return this.congressmen
+                .filter(member => member.party === partyCode)
+                .length
+        },
+
+
+        percentVoted(partyCode) {
+            var memberspercent = []
+            var members = this.congressmen.filter(member => member.party === partyCode)
+            for (var i = 0; i < members.length; i++) {
+                memberspercent.push(members[i].votes_with_party_pct)
+            }
+
+            memberspercent = memberspercent.reduce((a, b) => a + b, 0) / members.length
+            memberspercent = memberspercent.toFixed(2);
+            return memberspercent
+        },
+
+        totalPercentage() {
+            var membersPercentTotal = []
+            for (var i = 0; i < this.congressmen.length; i++) {
+                membersPercentTotal.push(this.congressmen[i].votes_with_party_pct)
+                var result = membersPercentTotal.reduce(function (a, b) {
+                    return a + b;
+                }, 0);
+            }
+            return result / this.congressmen.length
+        }
+    }
+})
